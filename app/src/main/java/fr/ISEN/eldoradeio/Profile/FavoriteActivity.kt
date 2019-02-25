e9365c44dba2444dd2849bf6d6a6d44b76353e31
Antoine PARANT <antoine.parant83@gmail.com>
2019-02-25 15:38:48 +0100

    Initial Commit Profile Renamed ProfilActivity to ProfileActivity

--- app/src/main/java/fr/ISEN/eldoradeio/Profile/FavoriteActivity.kt
+++ app/src/main/java/fr/ISEN/eldoradeio/Profile/FavoriteActivity.kt
@@ -1,12 +1,13 @@
-package fr.ISEN.eldoradeio
+package fr.ISEN.eldoradeio.Profile
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import fr.ISEN.eldoradeio.R
 
-class GroupActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group)
+        setContentView(R.layout.activity_favorite)
     }
 }
