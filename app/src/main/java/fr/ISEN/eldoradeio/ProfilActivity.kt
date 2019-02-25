f8060ca3e0981d2b462ac6b9e1cf9393dd4fa052
Antoine PARANT <antoine.parant83@gmail.com>
2019-02-25 15:19:27 +0100

    Initial Commit

---
+++ app/src/main/java/fr/ISEN/eldoradeio/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package fr.ISEN.eldoradeio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
---
+++ app/src/main/java/fr/ISEN/eldoradeio/RoomsActivity.kt
@@ -0,0 +1,12 @@
+package fr.ISEN.eldoradeio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rooms)
+    }
+}
