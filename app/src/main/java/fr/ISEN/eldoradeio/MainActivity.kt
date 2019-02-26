5677bc1b4f2c01501303459c171224f0c8240ad9
onceuponaban <onceuponaban@gmail.com>
2019-02-26 08:11:19 +0100

    Set up Firebase test

--- app/src/main/java/fr/ISEN/eldoradeio/MainActivity.kt
+++ app/src/main/java/fr/ISEN/eldoradeio/MainActivity.kt
@@ -2,11 +2,37 @@ package fr.ISEN.eldoradeio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private val mDatabase = FirebaseDatabase.getInstance()
+    private val mRoomReference = mDatabase.getReference("rooms")
+
+    companion object{
+
+        const val ROOM_FULLY_AVAILABLE = 0
+        const val ROOM_PARTIALLY_AVAILABLE = 1
+        const val ROOM_UNAVAILABLE = 2
+
+    }
+
+
+    data class Room(
+        val roomName: String = "",
+        val roomFloor: Int,
+        val availability: Int = ROOM_FULLY_AVAILABLE,
+        var uuid: String = "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testRoom = Room("501",5)
+        /*val key = mRoomReference.push().key!!
+        testRoom.uuid = key
+        mRoomReference.child(key).setValue(testRoom)*/
     }
 }
